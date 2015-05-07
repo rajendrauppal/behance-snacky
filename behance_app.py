34c8e137056e657fb4bf7a29bc05d596bc5e1516
rajendrauppal <rajen.iitd@gmail.com>
2015-05-07 14:34:50 +0530

    Behance python app

--- behance_app.py
+++ behance_app.py
@@ -13,7 +13,7 @@ __author__ = 'Rajendra Kumar Uppal'
 from behance_python.api import API
 
 
-API_KEY = 'JFkguN6GZLoxQcOg2vkohmkk0165tuaS'
+API_KEY = 'krFXz0JBgKT5OVYaVRrh9ouWEHSwqycu'
 
 
 def behance_client():
@@ -21,6 +21,19 @@ def behance_client():
     projects = behance.project_search('dog', 'cat', filter_key='')
     owner = projects[0].owners[0].first_name + ' ' + projects[0].owners[0].last_name
     print owner
+
+    users = behance.user_search('dude', '', filter_key='')
+    for user in users:
+        user_details = behance.get_user(user.username)
+        print user_details.first_name
+        
+        #user_projects = user_details.get_projects(filter_key='')
+        #for user_project in user_projects:
+        #    print user_project.name
+
+        user_wips = user_details.get_wips(filter_key='')
+        for user_wip in user_wips:
+            print '[WIP]: ' + user_wip.title
 
 
 def main():
